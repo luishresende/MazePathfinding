f26f96b4d6e22e2102f8df862a70c5a40f88fe1d
Luis Henrique da Silva Resende <luishresende@outlook.com.br>
2024-11-29 22:50:39 -0300

    Update Graph
    adjacents estava fora do escopo

--- Game/Graph.py
+++ Game/Graph.py
@@ -1,10 +1,10 @@
 class Graph:
-    adjs_list = []
     def __init__(self, terrain_type, matrix_position_x, matrix_position_y, reward=False):
         self.terrain_type = terrain_type
         self.matrix_position_x = matrix_position_x
         self.matrix_position_y = matrix_position_y
         self.reward = reward
+        self.adjacents = []
 
     def add_adjacent(self, adj):
-        self.adjs_list.append(adj)
+        self.adjacents.append(adj)
